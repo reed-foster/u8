05136d67393a09cf992c42beeb1a8c0f57a5536d
reed-foster <reed.a.foster@gmail.com>
2019-01-26 12:51:20 -0500

    made data bidirectional

--- rtl/memio/usbfifo/usbfifo.v
+++ rtl/memio/usbfifo/usbfifo.v
@@ -4,16 +4,14 @@
 // could make the interface a synch <-> asynch interface using a fifo
 
 module usbfifo
-    #( // parameters
-
-    )( // ports
+    ( // ports
         // FT2232H interface
         input rxf, // low when data is available to read
         input txe, // low when data can be written to fifo
         output rd, // active low
         output wr, // active low
-        input [7:0] rx_data,
-        output [7:0] tx_data,
+        inout [7:0] data,
+        output [7:0] leds,
 
         input clock // internal logic must be synchronous
     );
@@ -21,8 +19,11 @@ module usbfifo
     // architecture
 
     // increment received word by 1
+    wire [7:0] tx_data;
     reg [7:0] word;
-    assign tx_data = word + 8'b1;
+    assign data = rxf ? word + 8'b1 : 8'bZ; // output data when not reading
+    assign leds = word;
+
     // synchronize rxf and txe signals
     reg s_rxf = 1; // active low
     reg s_txe = 1; // active low
@@ -31,16 +32,16 @@ module usbfifo
 
     // trd (read -> data) is < 14ns so 50MHz clock is fine
     // tdw (data -> write) is > 5ns; 50MHz is definitely fine
-    always_ff @(posedge clock)
+    always @ (posedge clock)
     begin
         s_rxf <= rxf;
         s_txe <= txe;
     end
 
-    always_ff @(negedge clock)
+    always @ (negedge clock)
     begin
         if (s_rxf == 0) // s_rxf must've been low for half a clock period
-            word <= rx_data;
+            word <= data;
     end
 
-endmodule;
+endmodule
